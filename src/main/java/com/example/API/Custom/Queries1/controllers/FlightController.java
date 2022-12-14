ebc275958a79684b2460a4b901f7b9e20ca7cf0c
alessandro <alessandroserra233@gmail.com>
2022-12-14 11:15:32 +0100

    aggiunto getAllFlights

--- src/main/java/com/example/API/Custom/Queries1/controllers/FlightController.java
+++ src/main/java/com/example/API/Custom/Queries1/controllers/FlightController.java
@@ -4,6 +4,7 @@ import com.example.API.Custom.Queries1.entities.Flight;
 import com.example.API.Custom.Queries1.entities.Status;
 import com.example.API.Custom.Queries1.repositories.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,12 @@ public class FlightController {
             Flight f = new Flight(random.ints().toString(), random.ints().toString(), random.ints().toString(), st);
             flightRepository.save(f);
         }
+    }
+
+    @GetMapping("")
+    public List<Flight> getAllFlights(){
+        List<Flight> allFlights = new ArrayList<>();
+        allFlights.addAll(flightRepository.findAll());
+        return allFlights;
     }
 }
